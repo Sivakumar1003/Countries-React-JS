f98eb780e5b0069f1fe415fa951c552478be4f8a
Sivakumar N <shivask94423@gmail.com>
2025-02-22 15:10:33 +0530

    selector for region based on all countries.

--- src/components/RegionSelector.jsx
+++ src/components/RegionSelector.jsx
@@ -25,7 +25,7 @@ export default function RegionSelector({ countries, setSelectedRegion, selectedR
     }
 
     return (
-        <div className='flex flex-col justify-center gap-8 p-6 md:flex-row md:justify-between'>
+        <div className='flex flex-col justify-center gap-8 p-6 md:flex-row md:justify-between '>
 
             <div className={` ${darkMode ? darkElement : lightElement} shadow flex p-2 w-full m-auto md:m-0 md:w-[45%] max-w-[600px] gap-4 items-center rounded-lg`}>
                 <img src={`${darkMode ? '/searchdark.svg' : '/searchdark.svg' }`} className={`h-[30px] ${darkMode ? "" : ""}`} />
@@ -37,9 +37,8 @@ export default function RegionSelector({ countries, setSelectedRegion, selectedR
                     className={`${darkMode ? darkElement : lightElement} font-medium text-md md:text-lg w-full h-[30px] focus:border-0 cursor-pointer focus:outline-none`}
                 />
             </div>
-            {/* searchlight  */}
 
-            <div className={` ${darkMode ? darkElement : lightElement} shadow p-2 items-center w-fit lg:min-w-[250px] rounded-lg`}>
+            <div className={` ${darkMode ? darkElement : lightElement} shadow p-2 items-center w-[60%] md:max-w-[350px] rounded-lg`}>
 
                 <select
                     name="region"
