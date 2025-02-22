b17bed94e2575ec40b9e048af3f62319e3530a3e
Sivakumar N <shivask94423@gmail.com>
2025-02-22 15:08:22 +0530

    updated to each as link to show all details.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,36 +1,39 @@
 import React, { useContext } from 'react'
 import { themeContext } from '../App'
+import { Link } from 'react-router-dom';
 
 export default function Card({ country }) {
 
     const { darkMode, darkElement, lightElement } = useContext(themeContext);
 
     return (
-        <div className={`${darkMode ? darkElement : lightElement} w-full m-auto h-[400px] rounded-lg shadow-lg`}>
-            {
-                country &&
-                <div >
-                    <img src={`${country["flags"]["svg"]}`} alt="Invalid image" className='h-[200px] w-full object-cover object-center shadow-lg rounded-t-lg' />
+        <Link to={`country/${country["cca3"]}`}>
+            <div className={`${darkMode ? darkElement : lightElement} w-full m-auto h-[400px] rounded-lg shadow-lg`}>
+                {
+                    country &&
+                    <div >
+                        <img src={`${country["flags"]["svg"]}`} alt="Invalid image" className='h-[200px] w-full object-cover object-center shadow-lg rounded-t-lg' />
 
-                    <div className='p-5'>
-                        <p className='font-black text-base'>{country["name"]["common"]}</p>
-                        <div className='py-4'>
-                            <p>
-                                <span className='font-bold text-sm'>Population: </span>
-                                <span className='font-normal text-sm'>{country["population"] ? country["population"] : "N/A"}</span>
-                            </p>
-                            <p>
-                                <span className='font-bold text-sm'>Region: </span>
-                                <span className='font-normal text-sm'>{country["region"] ? country["region"] : "N/A"}</span>
-                            </p>
-                            <p>
-                                <span className='font-bold text-sm'>Capital: </span>
-                                <span className='font-normal text-sm'>{ country["capital"] ? country["capital"].join(", ") : "N/A"}</span>
-                            </p>
+                        <div className='p-5'>
+                            <p className='font-black text-base'>{country["name"]["common"]}</p>
+                            <div className='py-4'>
+                                <p>
+                                    <span className='font-bold text-sm'>Population: </span>
+                                    <span className='font-normal text-sm'>{country["population"] ? country["population"] : "N/A"}</span>
+                                </p>
+                                <p>
+                                    <span className='font-bold text-sm'>Region: </span>
+                                    <span className='font-normal text-sm'>{country["region"] ? country["region"] : "N/A"}</span>
+                                </p>
+                                <p>
+                                    <span className='font-bold text-sm'>Capital: </span>
+                                    <span className='font-normal text-sm'>{country["capital"] ? country["capital"].join(", ") : "N/A"}</span>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            }
-        </div>
+                }
+            </div>
+        </Link>
     )
 }
